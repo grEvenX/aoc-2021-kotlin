4199bb520c01783ca7dfa53f27791a46d0fe3abe
Even André Fiskvik <even@myworkout.no>
2021-12-01 10:00:33 +0100

    Day 1 solution

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,32 @@
 fun main() {
+    fun countLarger(list: List<Int>): Int {
+        return list.windowed(2, 1) {
+            val prevValue = it.getOrElse(0) { 0 }
+            val currentValue = it.getOrElse(1) { 0 }
+            if (currentValue > prevValue) {
+                1
+            } else {
+                0
+            }
+        }.sum()
+    }
+
     fun part1(input: List<String>): Int {
-        return input.size
+        return return countLarger(input.map { it.toInt() })
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val summedWindow = input.windowed(3, 1) {
+            it.sumOf { sub -> sub.toInt() }
+        }
+
+        return countLarger(summedWindow)
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 7)
+    check(part2(testInput) == 5)
 
     val input = readInput("Day01")
     println(part1(input))
